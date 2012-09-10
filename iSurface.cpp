b2751b10773d9b3d652476752bb7e946484e2db3
Josh Cates <josh.cates@gmail.com>
2012-09-10 11:33:54 -0600

    Working version

--- iSurface.cpp
+++ iSurface.cpp
@@ -1,3 +1,17 @@
+/** Copyright 2012 Joshua Cates cates@sci.utah.edu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+*/
 #include <iostream>
 #include <vtkSmartPointer.h>
 #include <vtkPolyData.h>
@@ -14,6 +28,7 @@
 #include <itkImageFileWriter.h>
 #include <itkAntiAliasBinaryImageFilter.h>
 #include <itkReinitializeLevelSetImageFilter.h>
+#include <itkDiscreteGaussianImageFilter.h>
 #include "itkImageToVTKImageFilter.h"
 
 int main (int argc, char *argv[])
@@ -21,20 +36,17 @@ int main (int argc, char *argv[])
   if (argc < 3)
     {
       std::cout << "Usage:" << argv[0] 
-		<< "input_name output_prefix [smoothing_parameter]" << std::endl;
+		<< "input_name output_prefix [display_result=1] [smoothing_sigma]" << std::endl;
       return 1;
     }
 
   double sigma = 0.0;
   double isosurface_value = 0.5;
+  int display_result = 0;
   if (argc >3)
-    {
-      
-    }
+   {   display_result = atoi(argv[3]);  }
   if (argc >4)
-    {
-
-    }
+    { sigma = atof(argv[4]);  }
 
   typedef itk::Image<float, 3> image_type;
 
@@ -42,14 +54,14 @@ int main (int argc, char *argv[])
     itk::ImageFileReader<image_type>::Pointer reader 
       = itk::ImageFileReader<image_type>::New();
     reader->SetFileName(argv[1]);
-
+    
     itk::AntiAliasBinaryImageFilter<image_type, image_type>::Pointer anti
       = itk::AntiAliasBinaryImageFilter<image_type, image_type>::New();
     anti->SetInput(reader->GetOutput());
-    anti->SetNumberOfIterations(100);
+    anti->SetNumberOfIterations(150);
     anti->SetMaximumRMSError(0.0);
     anti->SetIsoSurfaceValue(isosurface_value);
-
+     
     itk::ReinitializeLevelSetImageFilter<image_type>::Pointer filt
       = itk::ReinitializeLevelSetImageFilter<image_type>::New();
     filt->SetInput(anti->GetOutput());
@@ -57,15 +69,21 @@ int main (int argc, char *argv[])
     filt->SetLevelSetValue(0.0);
     filt->Update();
     
+    itk::DiscreteGaussianImageFilter<image_type, image_type>::Pointer blur
+      = itk::DiscreteGaussianImageFilter<image_type, image_type>::New();
+    blur->SetInput(filt->GetOutput());
+    blur->SetVariance(sigma * sigma);
+    blur->SetUseImageSpacingOn();
+    
     itk::ImageToVTKImageFilter<image_type>::Pointer importer 
       = itk::ImageToVTKImageFilter<image_type>::New();    
-    importer->SetInput(filt->GetOutput());
+    importer->SetInput(blur->GetOutput());
     importer->Update();
-
+    
     vtkSmartPointer<vtkImageToStructuredPoints> converter 
       = vtkSmartPointer<vtkImageToStructuredPoints>::New();
     converter->SetInput(importer->GetOutput());
-
+    
     vtkSmartPointer<vtkContourFilter> contour_filter 
       = vtkSmartPointer<vtkContourFilter>::New();
     contour_filter->SetInputConnection(converter->GetOutputPort());
@@ -80,7 +98,7 @@ int main (int argc, char *argv[])
     // fwriter->SetInput(filt->GetOutput());
     // fwriter->SetFileName("tmp.nrrd");
     // fwriter->Update();
-
+    
     vtkSmartPointer<vtkRenderer> ren1 = vtkSmartPointer<vtkRenderer>::New();
     vtkSmartPointer<vtkRenderWindow> renwin = vtkSmartPointer<vtkRenderWindow>::New();
     renwin->AddRenderer(ren1);
@@ -102,6 +120,9 @@ int main (int argc, char *argv[])
     writer->SetInput(renwin);
     writer->SetFilePrefix(argv[2]);
     writer->Update();
+    
+    if (display_result != 0)
+      { renderWindowInteractor->Start();  }
     
   }
   catch (itk::ExceptionObject &e)
